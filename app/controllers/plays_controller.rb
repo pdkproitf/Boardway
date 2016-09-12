235742bae0946d017a0db101687f01e79aefdce4
pdkproitf <phucdaica.pro@gMAIL.com>
2016-09-12 12:17:29 +0700

    add play to App, index , show action view

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,11 +1,16 @@
 class PlaysController < ApplicationController
+  before_action :find_play, only: [:show, :destroy, :edit, :update]
 
   def index
-    
+    @plays = Play.all.order('created_at DESC')
   end
 
   def new
     @play = Play.new
+  end
+
+  def show
+    
   end
 
   def create
@@ -18,6 +23,10 @@ class PlaysController < ApplicationController
   end
 
   private 
+    def find_play
+      @play = Play.find(params[:id])
+    end
+
     def play_params
       params.require(:play).permit(:title, :description, :director)
     end
