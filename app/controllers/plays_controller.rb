4769cea29b21587379ba604edc096aa6a84eb87c
pdkproitf <phucdaica.pro@gMAIL.com>
2016-09-12 11:55:17 +0700

    add new and create play

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -4,4 +4,21 @@ class PlaysController < ApplicationController
     
   end
 
+  def new
+    @play = Play.new
+  end
+
+  def create
+    @play = Play.new(play_params)
+    if @play.save
+      redirect_to @play
+    else
+      render 'new'
+    end
+  end
+
+  private 
+    def play_params
+      params.require(:play).permit(:title, :description, :director)
+    end
 end
