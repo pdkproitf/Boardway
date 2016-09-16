38cc8453940ea7d7b5f34111763ce8bf68922814
pdkproitf <phucdaica.pro@gmail,com>
2016-09-17 00:27:07 +0700

    add review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916152416) do
+ActiveRecord::Schema.define(version: 20160916172400) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20160916152416) do
     t.datetime "play_img_updated_at"
     t.index ["category_id"], name: "index_plays_on_category_id"
     t.index ["user_id"], name: "index_plays_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "play_id"
   end
 
   create_table "users", force: :cascade do |t|
